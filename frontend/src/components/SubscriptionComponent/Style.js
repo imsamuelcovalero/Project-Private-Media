e7b1c00cf5da8327788303451a83aa2ef673752f
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-08-21 22:40:21 -0300

    style(Subscription): Altera o estilo do parágrado do valor da mensalidade

--- frontend/src/components/SubscriptionComponent/Style.js
+++ frontend/src/components/SubscriptionComponent/Style.js
@@ -20,6 +20,13 @@ const SubscriptionS = styled.div`
     text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
   }
 
+  p {
+    font-size: 1.2rem;
+    color: var(--textColor);
+    margin-bottom: 1rem;
+    text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.1);
+  }
+
   button {
     display: block;
     margin: 1rem auto 0;
