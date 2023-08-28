5c85637fc85e1605b3338715c1c69e2b7c4ec7a5
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-08-28 19:08:00 -0300

    refactor(Backend): Remove pastas não utilizadas

--- backend/src/config/mercadopagoConfig.js
+++
@@ -1,8 +0,0 @@
-/* File: src/config/mercadopagoConfig.js */
-const mercadopago = require('mercadopago');
-
-console.log("Token do MercadoPago:", process.env.MERCADOPAGO_ACCESS_TOKEN);
-
-mercadopago.configurations.setAccessToken(process.env.MERCADOPAGO_ACCESS_TOKEN);
-
-module.exports = mercadopago;
